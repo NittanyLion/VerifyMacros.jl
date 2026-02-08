c8ff88bec1bfd257239986b0ddeea4fb2049daca
Joris Pinkse <pinkse@gmail.com>
2026-02-08 18:21:27 -0500

    removed extraneous junk

--- macro_test.jl
+++
@@ -1,8 +0,0 @@
-macro check_args(action, args...)
-    println("Action type: ", typeof(action))
-    println("Action: ", action)
-    println("Args: ", args)
-    return nothing
-end
-
-@check_args :keys 1 2 3
